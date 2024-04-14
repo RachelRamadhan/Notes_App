5777f8f73d14d4cb1b3076d1dbd4bee18569a0ef
Rachel Ramadhan <rachelramadhann@gmail.com>
2024-04-14 23:23:32 +0700

    merubah warna button pada view note activity dan update note activity. membuat callback untuk swipe to delete.

---
+++ app/src/main/java/com/rachel/notes/SwipeToDeleteCallback.kt
@@ -0,0 +1,42 @@
+package com.rachel.notes
+
+import android.graphics.Canvas
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
+
+abstract class SwipeToDeleteCallback : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT){
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        val swipeFlag = ItemTouchHelper.LEFT
+
+        return  makeMovementFlags(0, swipeFlag)
+    }
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
+
+    override fun onChildDraw(
+        c: Canvas,
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        dX: Float,
+        dY: Float,
+        actionState: Int,
+        isCurrentlyActive: Boolean
+    ) {
+        RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+            .addSwipeLeftBackgroundColor(R.color.red)
+            .addSwipeLeftActionIcon(R.drawable.ic_delete)
+            .create()
+            .decorate()
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+}
